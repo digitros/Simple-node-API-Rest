cf8761184e61526b100fac8f1068c3a3a8080136
digitros <dgogo9@hotmail.com>
2020-09-11 13:33:23 -0500

    Creating routes

--- router.js
+++ router.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
+const movieController = require('./movieController');
 
 router.get('/', function(req, res) {
     res.json({
-        status: 'Succesfull',
-        message: 'Welcome to the API'
+        status: 'Success',
+        message: 'Welcome to the Movie API'
     });
 });
+
+router.route('/movie')
+    .get(movieController.index)
+    .post(movieController.add);
+router.route('/movie/:movie_id')
+    .get(movieController.view)
+    .patch(movieController.update)
+    .put(movieController.update)
+    .delete(movieController.delete);
 
 module.exports = router;
